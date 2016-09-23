4f3697e9c8e4a69c34ea2d692579cd51ff47de2a
Chih-Yung Liang <stu89424@gmail.com>
2016-09-23 14:11:14 +0800

    Right-side-left

--- hw1.py
+++ hw1.py
@@ -12,9 +12,23 @@ def upside_down(img):
 	result.putdata(data.data)
 	result.save('results/upside_down.bmp')
 
+def right_side_left(img):
+	data = Pixels2D(img)
+
+	for y in range(img.height):
+		for x in range(img.width / 2):
+			data[x, y], data[img.width - 1 - x, y] = data[img.width - 1 - x, y], data[x, y]
+
+	result = Image.new(img.mode, img.size)
+	result.putdata(data.data)
+	result.save('results/right_side_left.bmp')
+
 if __name__ == '__main__':
 	img = Image.open('benchmarks/lena.bmp')
 	
 	# 1
 	upside_down(img)
+
+	# 2
+	right_side_left(img)
 
