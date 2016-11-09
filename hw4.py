79d4a7efc6300a57404756e45db68692734275ae
Chih-Yung Liang <stu89424@gmail.com>
2016-11-09 20:47:33 +0800

    Make BinaryMorphology a class.

--- hw4.py
+++ hw4.py
@@ -2,28 +2,36 @@ from PIL import Image
 from hw2 import thresholding
 from helpers.image import PixelSet
 
-def dilation(a, b):
-    width, height = a.size
-    result = PixelSet(filter(lambda (x, y): 0 <= x < width and 0 <= y < height, [(ax + bx, ay + by) for (ax, ay) in a for (bx, by) in b]))
-    result.size, result.origin = a.size, a.origin
-    return result
+class BinaryMorphology(object):
+    # Arguments are expected to be `PixelSet`
 
-def erosion(a, b):
-    width, height = a.size
-    result = PixelSet([(x, y) for x in range(width) for y in range(height) if all([(x + bx, y + by) in a for (bx, by) in b])])
-    result.size, result.origin = a.size, a.origin
-    return result
+    @classmethod
+    def dilation(cls, a, b):
+        width, height = a.size
+        result = PixelSet(filter(lambda (x, y): 0 <= x < width and 0 <= y < height, [(ax + bx, ay + by) for (ax, ay) in a for (bx, by) in b]))
+        result.size, result.origin = a.size, a.origin
+        return result
 
-def opening(b, k):
-    return dilation(erosion(b, k), k)
+    @classmethod
+    def erosion(cls, a, b):
+        width, height = a.size
+        result = PixelSet([(x, y) for x in range(width) for y in range(height) if all([(x + bx, y + by) in a for (bx, by) in b])])
+        result.size, result.origin = a.size, a.origin
+        return result
 
-def closing(b, k):
-    return erosion(dilation(b, k), k)
+    @classmethod
+    def opening(cls, b, k):
+        return cls.dilation(cls.erosion(b, k), k)
 
-def hit_and_miss(a, j, k):
-    result = erosion(a, j) & erosion(a.complement, k)
-    result.size, result.origin = a.size, a.origin
-    return result
+    @classmethod
+    def closing(cls, b, k):
+        return cls.erosion(cls.dilation(b, k), k)
+
+    @classmethod
+    def hit_and_miss(cls, a, j, k):
+        result = cls.erosion(a, j) & cls.erosion(a.complement, k)
+        result.size, result.origin = a.size, a.origin
+        return result
 
 if __name__ == '__main__':
     img = Image.open('benchmarks/lena.bmp')
@@ -37,10 +45,10 @@ if __name__ == '__main__':
         0, 1, 1, 1, 0,
     ], size=(5, 5), value=1, origin=(2, 2))
 
-    dilation(white_set, oct_kernel).to_image().save('results/dilation.bmp')
-    erosion(white_set, oct_kernel).to_image().save('results/erosion.bmp')
-    opening(white_set, oct_kernel).to_image().save('results/opening.bmp')
-    closing(white_set, oct_kernel).to_image().save('results/closing.bmp')
+    BinaryMorphology.dilation(white_set, oct_kernel).to_image().save('results/dilation.bmp')
+    BinaryMorphology.erosion(white_set, oct_kernel).to_image().save('results/erosion.bmp')
+    BinaryMorphology.opening(white_set, oct_kernel).to_image().save('results/opening.bmp')
+    BinaryMorphology.closing(white_set, oct_kernel).to_image().save('results/closing.bmp')
 
     j = PixelSet.from_pixels([
         0, 0, 0, 0, 0,
@@ -57,5 +65,5 @@ if __name__ == '__main__':
         0, 0, 0, 0, 0,
     ], size=(5, 5), value=1, origin=(1, 2))
 
-    hit_and_miss(white_set, j, k).to_image().save('results/hit_and_miss.bmp')
+    BinaryMorphology.hit_and_miss(white_set, j, k).to_image().save('results/hit_and_miss.bmp')
 
